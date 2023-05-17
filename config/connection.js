3c6b1ee70d27f4345b719cf86d1c2d46505bb936
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-05-16 22:41:58 -0700

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-let sequeliz;
+let sequelize;
 
     
 if (process.env.JAWSDB_URL) {
@@ -11,13 +11,12 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306,
     },
   );
 }
-
 module.exports = sequelize;
